ccad57c08950d7d879fb8241872e820a686ba2dd
Prabhukorenor <prabhukorenor@gmail.com>
2026-05-18 12:31:58 +0530

    second commit

--- src/main/java/com/example/ecommerce/entity/User.java
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -11,17 +11,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name= "users")
 @Data
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 
 
